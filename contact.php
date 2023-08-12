d87a40c6600c5303979e9938a457517c7e107ea7
leonardo-travagini-delboni <leonardodelboni@gmail.com>
2023-08-12 01:36:23 -0300

    background update + favicon fixed + contact.php being tested + contact page being created

---
+++ contact.php
@@ -0,0 +1,30 @@
+<?php
+// Verifica a resposta anti-spam
+if ($_POST['spam_check'] != '8') {
+    echo "Verificação anti-spam incorreta!";
+    exit;
+}
+
+// Receptores
+$to = 'leonardodelboni@gmail.com', 'leonardo@leonardodelboni.com.br';
+
+// Assunto
+$subject = $_POST['subject'];
+
+// Mensagem
+$message = 'Nome: ' . $_POST['name'] . "\n";
+$message .= 'Email: ' . $_POST['email'] . "\n";
+$message .= 'Mensagem: ' . $_POST['message'];
+
+// Cabeçalhos
+$headers = 'From: webmaster@example.com' . "\r\n" .
+    'Reply-To: ' . $_POST['email'] . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+// Enviar o email
+if (mail($to, $subject, $message, $headers)) {
+    echo "Mensagem enviada com sucesso!";
+} else {
+    echo "Erro ao enviar a mensagem.";
+}
+?>
