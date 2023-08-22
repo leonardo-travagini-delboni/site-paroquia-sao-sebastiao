1e1b77805070dd51a7c70b2362c98cf1fcac8017
leonardo-travagini-delboni <leonardodelboni@gmail.com>
2023-08-22 18:06:17 -0300

    test send_email 46

--- config/parameters.php
+++ config/parameters.php
@@ -1,22 +1,16 @@
 <?php
-    // Importing envoirement variables (composer require vlucas/phpdotenv):
-    require 'vendor/autoload.php';
-    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-    $dotenv->load();
-
     // Database connections settings:
-    $db_server = getenv('DB_SERVER');
-    $db_user = getenv('DB_USER');
-    $db_pass = getenv('DB_PASS');
-    $db_name = getenv('DB_NAME');
+    $db_server = "127.0.0.1:3306";
+    $db_user = "u826974739_paroquia";
+    $db_pass = "tgzik}%@h.=6XdZJ7Z";
+    $db_name= "u826974739_saosebastiao";
 
     // Global repo parameters:
-    $contact_receiver = getenv('CONTACT_RECEIVER');
+    $contact_receiver = "contato@saosebastiaoparoquia.com.br";
 
     // QR CODE copia e cola:
-    $pix_qr_code_codigo = getenv('PIX_QR_CODE');
-    $pix_qr_code_nome = getenv('PIX_NOME');
-    $pix_qr_code_agencia = getenv('PIX_BANCO');
-    $pix_qr_code_cnpj = getenv('PIX_CNPJ');
-    
+    $pix_qr_code_codigo = "00020126360014BR.GOV.BCB.PIX0114037073580033245204000053039865802BR5925PAROQUIA DE SAO SEBASTIAO6014NOVO HORIZONTE62070503***63040CC1";
+    $pix_qr_code_nome = "PAROQUIA DE SAO SEBASTIAO";
+    $pix_qr_code_agencia = "CAIXA ECONOMICA FEDERAL";
+    $pix_qr_code_cnpj = "03.707.358/0033-24";
 ?>
