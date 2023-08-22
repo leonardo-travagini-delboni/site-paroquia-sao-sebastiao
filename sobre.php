7cd55510aa24e2505b97a2cd03a649e7c34bd931
leonardo-travagini-delboni <leonardodelboni@gmail.com>
2023-08-22 16:50:27 -0300

    test send_email 43

--- sobre.php
+++ sobre.php
@@ -1,6 +1,6 @@
 <?php
     // Naming this website page:
-    $pageTitle = "Paróquia São Sebastião - Pastorais";
+    $pageTitle = "Paróquia São Sebastião - Sobre";
 
     // Including the header:
     include("models/header.php");
@@ -15,11 +15,11 @@
     <!-- Customized Page Header Start -->
     <div class="container-fluid page-header py-5 mb-5">
         <div class="container py-5">
-            <h1 class="display-3 text-white mb-3 animated slideInDown">PASTORAIS</h1>
+            <h1 class="display-3 text-white mb-3 animated slideInDown">SOBRE</h1>
             <nav aria-label="breadcrumb animated slideInDown">
                 <ol class="breadcrumb">
                     <li class="breadcrumb-item"><a class="text-white" href="index.php">INÍCIO</a></li>
-                    <li class="breadcrumb-item text-white active" aria-current="page">PASTORAIS</li>
+                    <li class="breadcrumb-item text-white active" aria-current="page">SOBRE NÓS</li>
                 </ol>
             </nav>
         </div>
@@ -30,8 +30,8 @@
 </html>
 
 <?php
-    // Importing the churches forms:
-    include("models/associations.php");
+    // Importing the about us:
+    include("models/about_us.php");
 ?>
 
 <?php
