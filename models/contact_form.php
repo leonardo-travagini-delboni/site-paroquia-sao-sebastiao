813032bac2393985f8cf08272b20378068f6974e
leonardo-travagini-delboni <leonardodelboni@gmail.com>
2023-08-22 01:02:15 -0300

    test send_email 16

--- models/contact_form.php
+++ models/contact_form.php
@@ -133,11 +133,25 @@
                         mysqli_query($conn, $query_new_row);
                         // ATÉ AQUI ESTÁ FUNCIONANDO
 
-                        // Importing the e-mail sending funtion:
-                        include("functions/send_email.php");
+                        // Conteúdo do e-mail
+                        $email_content = null;
+                        $email_content = "Nome: $contact_name\n";
+                        $email_content .= "Email: $contact_email\n";
+                        $email_content .= "Telefone: $contact_telephone\n";
+                        $email_content .= "Mensagem:\n$contact_message\n";
+                    
+                        // Cabeçalho do e-mail
+                        $headers = "From: $contact_name <$contact_email>";
+                
+                        // Enviar e-mail
+                        if (mail($contact_receiver, $contact_subject, $email_content, $headers)) {
+                            header("Location: " . $_SERVER['PHP_SELF']);
+                            exit;
+                        } 
+                        else {
+                            echo "Ocorreu um erro ao enviar o e-mail.";
+                        }
 
-                        // Sending the e-mail:
-                        sendEmail($contact_name, $contact_email, $contact_telephone, $contact_receiver, $contact_subject, $contact_message, $contact_ip);
                         }
                     }
                 }
